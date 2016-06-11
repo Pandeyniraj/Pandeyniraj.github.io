8b230bda71d4269dcfc42970a0001bdac2fd21cd
Pandeyniraj <niraj.pandey1619@gmail.com>
2016-06-11 06:32:40 +0000

    11-6-2016

---
+++ mymodule.js
@@ -0,0 +1,13 @@
+/**
+ * Created by student on 6/11/2016.
+ */
+var SayHello = function () 
+{
+  console.log("Hello How R You !");
+} 
+var SayGoodBye = function ()
+{
+    console.log("Hello  Say Good Bye!");
+}
+exports.SayHello = SayHello;
+exports.SayGoodBye = SayGoodBye;
